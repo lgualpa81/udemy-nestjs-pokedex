cb752effc4ec2745f21ff378dde7684d658f4c1c
Leonardo Gualpa <lgualpa@gmail.com>
2023-01-16 11:37:18 -0500

    insert multiple con insertMany

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -18,20 +18,20 @@ export class SeedService {
     await this.pokemonModel.deleteMany({});
 
     const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=10',
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
-    const insertPromisesArray = [];
+    const pokemonToInsert: { name: string; no: number }[] = [];
 
     data.results.forEach(({ name, url }) => {
       const segments: string[] = url.split('/');
       const no: number = +segments[segments.length - 2];
 
       //const pokemon = await this.pokemonModel.create({ name, no });
-      insertPromisesArray.push(this.pokemonModel.create({ name, no }));
+      pokemonToInsert.push({ name, no });
     });
 
-    await Promise.all(insertPromisesArray);
+    await this.pokemonModel.insertMany(pokemonToInsert);
 
     return 'Seed executed';
   }
